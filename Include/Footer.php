a3583d88406c0a0b6050debac8ac6b61db39875b
Gozabouro <elkhouryf@gmail.com>
2021-03-04 17:29:54 +0100

    Création du code HTML du header et du footer à l'intérieur des fichiers php pour affichage à travers les différentes pages du site. Ajout du logo GBAF à l'intérieur du header.

--- Include/Footer.php
+++ Include/Footer.php
@@ -1,4 +1,8 @@
-
+        <footer>
+            <a href="#">Mentions légales</a>
+            ¦
+            <a href="#">Contact</a>
+        </footer>
 
 </body>
 </html>
--- Include/Header.php
+++ Include/Header.php
@@ -2,6 +2,13 @@
     <html lang="en">
         <head>
             <meta charset="UTF-8">
-            <title>Title</title>
+            <title>Groupement Banque-Assurance Français</title>
+            <link rel="stylesheet" href="/CSS/style.css">
         </head>
     <body>
+            <nav>
+                <div>
+                    <img src="/Images/logo.png" alt="Groupement Banque-Assurance Français">
+                </div>
+                <div id="user"></div>
+            </nav>
