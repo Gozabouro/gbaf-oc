4721054756797d2235705dc5eda540f1a6e42882
Gozabouro <elkhouryf@gmail.com>
2021-03-04 18:54:12 +0100

    Création des différentes pages concernant la gestion utilisateur avec définition des conditions nécessaires

--- User.php
+++ User.php
@@ -1,3 +1,39 @@
-<?php
+        <?php
 include "Include/Header.php";
+    $page = $_GET['page'];
+    if (empty($page))
+    {
+        ?>
+        <h1>Connexion</h1>
+        <a href="User.php?page=Forgotten">Mot de passe oublié ?</a>
+        <p>Vous n'avez pas de compte ? <a href="User.php?page=Inscription">S'inscrire</a></p>
+        <?php
+    }
+    if ($page=="Inscription")
+    {
+        ?>
+        <h1>Inscription</h1>
+        <?php
+    }
+    if ($page=="Forgotten")
+    {
+        ?>
+        <h1>Mot de passe oublié</h1>
+
+        <?php
+        if (empty($username) && empty($answer))
+        {
+            // demande identifiant
+        }
+        if ($username==true)
+        {
+            // demande de la réponse à la question
+        }
+        if ($answer==true)
+        {
+            // changement de mot de passe possible
+        }
+
+    }
+
 include "Include/Footer.php";
